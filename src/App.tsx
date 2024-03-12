0194be4d7c3f1d5607fc3d1e7b62350dfd22daf8
leonelborgesdev <leonel03t@gmail.com>
2024-03-12 11:28:44 -0400

    hooks types.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useFetchRepositories } from './hooks/useRepos'
 
 function App() {
+  const {data, isLoading}=useFetchRepositories();
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  console.log(data)
   return (
-    <div>App</div>
+    <div>{data?.map((repo)=>(
+      <div key={repo.id}><h1>{repo.name}</h1></div>
+    ))}</div>
   )
 }
 
