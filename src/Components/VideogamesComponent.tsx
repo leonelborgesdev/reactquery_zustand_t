51b66ec76ef6137d2024f777e4665d3283008ce1
leonelborgesdev <leonel03t@gmail.com>
2024-03-27 11:44:53 -0400

    list allgenres, list allvideogames

--- src/Components/VideogamesComponent.tsx
+++ src/Components/VideogamesComponent.tsx
@@ -1,5 +1,6 @@
 import { useFetchGenres } from "../hooks/rootGeneres";
 import { useFetchRootVideogames } from "../hooks/rootVideogames"
+import { Genre, Videogame } from "../hooks/types";
 
 export const VideogamesComponent = () => {
     // const {data, isLoading}= useFetchRootVideogames();
@@ -12,9 +13,18 @@ export const VideogamesComponent = () => {
     }
     console.log(listvideogames)
     return (
-        <div>
-            <div></div>
-            <div></div>
+        <div className="container_videogames">
+            <div>{listgenres?.map((genre: Genre)=>{
+                return (
+                    <h4 key={genre.id}>{genre.nombre}</h4>
+                )
+            })            
+}           </div>
+            <div>{listvideogames?.map((videogame: Videogame)=>{
+                return(
+                    <h2 key={videogame.id}>{videogame.nombre}</h2>
+                )
+            })}</div>
         </div>
     )
 }
